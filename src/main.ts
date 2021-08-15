0a2bd615853b2db689d5d9bf57d9ec1ac306e143
wangtongmeng <tongmengwang@163.com>
2021-08-16 06:36:59 +0800

    安装normalize.css

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 初始化css
+import 'normalize.css/normalize.css'
+// element-plus
 import installElementPlus from './plugins/element'
 
 const app = createApp(App)
 
 app
-.use(store)
-.use(router)
-.use(installElementPlus)
-.mount('#app')
+  .use(store)
+  .use(router)
+  .use(installElementPlus)
+  .mount('#app')
