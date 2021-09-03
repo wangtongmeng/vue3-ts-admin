acb3057a6d9cc938f8a08818bdad4ed62c7cdf62
wangtongmeng <tongmengwang@163.com>
2021-09-03 13:50:55 +0800

    使用vuex

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="navbar">
-    <hambuger  @toggleClick="toggleSidebar" :is-active="true"/>
+    <hambuger  @toggleClick="toggleSidebar" :is-active="sidebar.opened"/>
     <breadcrumb />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
 import Breadcrumb from '@/components/Breadcrumb/index.vue'
 import Hambuger from '@/components/Hambuger/index.vue'
+import { useStore } from '@/store/index'
 
 export default defineComponent({
   name: 'Navbar',
@@ -17,12 +18,18 @@ export default defineComponent({
     Hambuger
   },
   setup() {
+    // 使用我们自定义的useStore 具备类型提示
+    // store.state.app.sidebar 对于getters里的属性没有类型提示
+    const store = useStore()
     const toggleSidebar = () => {
-      console.log('click')
+      store.dispatch('app/toggleSidebar')
     }
+    // 从getters中获取sidebar
+    const sidebar = computed(() => store.getters.sidebar)
 
     return {
-      toggleSidebar
+      toggleSidebar,
+      sidebar
     }
   }
 })
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <!-- 测试展开收起 -->
-    <h6 @click="isCollapse=!isCollapse">展收测试</h6>
     <el-menu
       class="sidebar-container-menu"
       mode="vertical"
@@ -26,9 +24,9 @@
 import { defineComponent, computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import variables from '@/styles/variables.scss'
-// 导入路由表
 import { routes } from '@/router'
 import SidebarItem from './SidebarItem.vue'
+import { useStore } from '@/store'
 
 export default defineComponent({
   name: 'Sidebar',
@@ -36,27 +34,23 @@ export default defineComponent({
     SidebarItem
   },
   setup() {
-    const route = useRoute() // 等价于 this.$route
+    const route = useRoute()
+    const store = useStore()
     // 根据路由路径 对应 当前激活的菜单
     const activeMenu = computed(() => {
-      const { path, meta } = route
-      // 可根据meta.activeMenu指定 当前路由激活时 让哪个菜单高亮选中
-      if (meta.activeMenu) {
-        return meta.activeMenu
-      }
+      const { path } = route
       return path
     })
     // scss变量
     const scssVariables = computed(() => variables)
-    // 展开收起状态 稍后放store
-    const isCollapse = ref(false)
+    // 展开收起状态 稍后放store 当前是展开就让它收起
+    const isCollapse = computed(() => !store.getters.sidebar.opened)
 
     // 渲染路由
     const menuRoutes = computed(() => routes)
 
     return {
-      // 不有toRefs原因 缺点在这里 variables里面变量属性感觉来源不明确 不知道有哪些变量值
-      // ...toRefs(variables),
+      // ...toRefs(variables), // 不有toRefs原因 缺点variables里面变量属性来源不明确
       scssVariables,
       isCollapse,
       activeMenu,
