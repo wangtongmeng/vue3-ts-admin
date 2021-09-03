2d070090d43f5dc1d2c3f11a327691e1c5231758
wangtongmeng <tongmengwang@163.com>
2021-09-03 10:48:22 +0800

    菜单收缩按钮

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="navbar">
+    <hambuger  @toggleClick="toggleSidebar" :is-active="true"/>
     <breadcrumb />
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
 import Breadcrumb from '@/components/Breadcrumb/index.vue'
+import Hambuger from '@/components/Hambuger/index.vue'
 
-export default {
+export default defineComponent({
   name: 'Navbar',
   components: {
-    Breadcrumb
+    Breadcrumb,
+    Hambuger
+  },
+  setup() {
+    const toggleSidebar = () => {
+      console.log('click')
+    }
+
+    return {
+      toggleSidebar
+    }
   }
-}
+})
 </script>
